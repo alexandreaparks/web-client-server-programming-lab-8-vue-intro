3441ce9a06bd84ba9f4fa97d818bb5393c158e20
alexandrea parks <alparks@idk.com>
2024-03-20 17:51:58 -0500

    Completed lecture videos 1 - 5

---
+++ function_definitions.js
@@ -0,0 +1,25 @@
+// function printMessage(message) {
+//     console.log('Your message is: ' + message)
+// }
+
+// const printMessage = function (message) {
+//     console.log('Your message is: ' + message)
+// }
+
+const printMessage = (message) =>  {
+    console.log('Your message is: ' + message)
+}
+
+printMessage('hello')
+printMessage('goodbye')
+
+
+// function printWelcome() {
+//     console.log('Welcome!')
+// }
+
+const printWelcome = () => {
+    console.log('Welcome!')
+}
+
+printWelcome()
